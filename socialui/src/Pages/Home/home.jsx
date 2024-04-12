92c24bd48715fe43f86a0cc212bf90f023ef2e31
Ashutosh Mishra <catchashu140695@gmail.com>
2024-04-12 09:31:29 +0530

    ok

--- socialui/src/Pages/Home/home.jsx
+++ socialui/src/Pages/Home/home.jsx
@@ -6,16 +6,14 @@ const home = () => {
 
     <div className="box">
       <div className="logo">
-        <iframe src="https://lottie.host/embed/de220bd8-9497-4e70-840f-fec8c67e7969/zMK2myTfhp.json"></iframe>
+        <iframe title='logo' src="https://lottie.host/embed/de220bd8-9497-4e70-840f-fec8c67e7969/zMK2myTfhp.json"></iframe>
         <p><b>Note:-</b>Just paste the youtube url and click on the submit button to download the video.</p>
       </div>
       <div className="form">
         <input type='text' className='input-field' placeholder='Youtube URL'></input>
         <button className='button-success'>Submit</button>
       </div>
-      <div className="table">
-
-      </div>
+      
     </div>
 
   )
